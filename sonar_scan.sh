b5230faffeb9bd56a5ff298630ecbff58314e1fb
xroot <xroot@pho-lap-02.lan>
2020-08-07 11:03:30 -0400

    updated sonarqube version to 7.9, updated sonar scan script

--- sonar_scan.sh
+++ sonar_scan.sh
@@ -2,4 +2,4 @@ sudo /home/xroot/sonar-scanner/sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner 
   -Dsonar.projectKey=devops-project \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://100.16.0.53:9000 \
-  -Dsonar.login=9440189740a0d69f48ec604b68f82fe4221e6854
+  -Dsonar.login=ee00f6c535ff91c13be48b8b88a3985759d41030
