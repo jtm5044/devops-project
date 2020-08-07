2d14a21edf0caf49f1b3611cb6226ddf46e49abe
xroot <xroot@pho-lap-02.lan>
2020-08-07 08:04:23 -0400

    updated sonar-scan script

--- sonar_scan.sh
+++ sonar_scan.sh
@@ -1,4 +1,4 @@
-sonar-scanner \
+/home/xroot/sonar-scanner/sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner \
   -Dsonar.projectKey=devops-project \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://100.16.0.53:9000 \
