dcc918f4abc7f54be872397e2f8b14a30de5440d
xroot <xroot@pho-lap-02.lan>
2020-08-07 11:43:16 -0400

    updated script to chmod gitlab-runner home dir

--- sonar_scan.sh
+++ sonar_scan.sh
@@ -1,3 +1,4 @@
+sudo chmod -R 777 /home/gitlab-runner/
 sudo /home/xroot/sonar-scanner/sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner \
   -Dsonar.projectKey=devops-project \
   -Dsonar.sources=. \
