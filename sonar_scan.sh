1903f12694158b666d89732807071b9f7801cc18
xroot <xroot@pho-lap-02.lan>
2020-08-07 11:50:11 -0400

    removed scannerwork manually

--- sonar_scan.sh
+++ sonar_scan.sh
@@ -1,5 +1,7 @@
-/home/xroot/sonar-scanner/sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner \
+sudo /home/xroot/sonar-scanner/sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner \
   -Dsonar.projectKey=devops-project \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://100.16.0.53:9000 \
   -Dsonar.login=ee00f6c535ff91c13be48b8b88a3985759d41030
+
+# sudo rm -R /home/gitlab-runner/builds/JxncWFfe/0/jmart204/devops-project/.scannerwork/
