f2dd14f574545076b2aed42db66892eb1e1cd2eb
xroot <xroot@pho-lap-02.lan>
2020-08-07 11:47:30 -0400

    trying sonar-scanner without sudo

--- sonar_scan.sh
+++ sonar_scan.sh
@@ -1,5 +1,4 @@
-sudo chmod -R 777 /home/gitlab-runner/
-sudo /home/xroot/sonar-scanner/sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner \
+/home/xroot/sonar-scanner/sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner \
   -Dsonar.projectKey=devops-project \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://100.16.0.53:9000 \
